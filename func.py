67280a1263b9c80fada62edd25f3c71d0b202231
Honey <honeysaxena525@gmail.com>
2023-05-05 20:56:40 +0530

    added css template

---
+++ func.py
@@ -0,0 +1,23 @@
+import cProfile
+import pstats
+import time
+
+def do_stuff():
+     
+    # single line to find factorial
+    result = []
+    for i in range(10000000):
+        result.append(i ** 2)
+    return result    
+
+
+if __name__ == "__main__":
+    with cProfile.Profile() as profile:
+        start = time.time()
+        print("Cameron:", do_stuff())
+        end = time.time()
+        print(end - start)
+
+    results = pstats.Stats(profile)
+    results.sort_stats(pstats.SortKey.TIME)
+    results.print_stats()
