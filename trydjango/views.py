3e7878c3dbe29bf92d99b59e3212c477514ac9de
Honey <honeysaxena525@gmail.com>
2023-05-06 12:17:53 +0530

    models.py added

--- trydjango/views.py
+++ trydjango/views.py
@@ -4,9 +4,17 @@ from ywcoffee.models import Article
 
 def home_view(request):
 
-    name = "Justin"
-    number = random.randint(10, 127685373)
+    number = random.randint(1, 3)
+
+    article_obj = Article.objects.get(id=number)
+    context = {
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content
+    }
+
     HTML_STRING = f"""
-    <h1>Hello {name} - {number}</h1>
-    """
+    <h1>{article_obj.title} - {article_obj.id}</h1>
+    <p>{article_obj.content}!</p>
+    """.format(**context)
     return HttpResponse(HTML_STRING)
